1db9b9346c576cccfee0290a51b58b2cd7a8cf5a
Akshit Yadav <akshit07032001@gmail.com>
2025-11-08 13:37:45 +0530

    Revert "Added .env.example"
    This reverts commit fca414fbfd51183277f81fab9bd025f34ece4c95.

--- client/src/services/index.ts
+++
@@ -1,3 +0,0 @@
-export { shapeService } from './shape.service';
-export { API_BASE_URL, API_ENDPOINTS, ApiError } from './api.config';
-export type { ApiResponse, BackendShape, ShapesResponse } from '@/types/api';
